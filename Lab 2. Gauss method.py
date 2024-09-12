57fd8bb5c19f5f8439c1bf6b8cf7eb1af7e54057
Zhanserik <zanserikzenisov@gmail.com>
2024-09-12 12:35:16 +0500

    Add files via upload

---
+++ Lab 2. Gauss method.py
@@ -0,0 +1,37 @@
+def gaussian_elimination(A, b):
+    n = len(b)
+
+    for i in range(n):
+        max_row = i
+        for k in range(i + 1, n):
+            if abs(A[k][i]) > abs(A[max_row][i]):
+                max_row = k
+
+        A[i], A[max_row] = A[max_row], A[i]
+        b[i], b[max_row] = b[max_row], b[i]
+
+        for k in range(i + 1, n):
+            factor = A[k][i] / A[i][i]
+            for j in range(i, n):
+                A[k][j] -= factor * A[i][j]
+            b[k] -= factor * b[i]
+
+    x = [0 for _ in range(n)]
+    for i in range(n - 1, -1, -1):
+        x[i] = b[i] / A[i][i]
+        for k in range(i - 1, -1, -1):
+            b[k] -= A[k][i] * x[i]
+
+    return x
+
+
+A = [
+    [1, 2, -1],
+    [2, -1, 3],
+    [1, 1, 1]
+]
+
+b = [3, 15, 6]
+
+solution = gaussian_elimination(A, b)
+print("Решение:", solution)
